5a13b02fa47cfb99f80a5221050f14f168e8c0e5
JesusVargasPacheco <jesus.vargas@iteso.mx>
2025-01-27 10:16:18 -0600

    Agregar archivo sintaxis_de_un_apuntador.c con explicación sobre apuntadores en C

---
+++ Porgramas de clase/C/sintaxis_de_un_apuntador.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*Ya sabemos cómo recuperar una dirección mediante el operador "dirección de". Ahora, obtengamos una variable
+para almacenar dicha dirección. Esta variable que es capaz de almacenar y operar con direcciones de variables, se
+llama apuntador. A continuación, se muestra la forma genérica mediante la cual se declaran apuntadores:*/
+
+
+datatype* <nombre_de_la_variable>; // Declaración de un apuntador
+
+<nombre_de_la_variable> = &<variable>; // Asignación de la dirección de memoria de una variable a un apuntador
+
