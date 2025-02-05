971bff5aa5f2bf55621476ffde15526632892ef4
JesusVargasPacheco <jesus.vargas@iteso.mx>
2025-02-05 10:40:00 -0600

    Actualizar comentarios en el programa de apuntadores en C para mejorar la claridad sobre el uso de punteros y la representación de datos en memoria

--- Porgramas de clase/C/programa_de_apuntadores.c
+++ Porgramas de clase/C/programa_de_apuntadores.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    int data=9;
-    int *iptr;
-    char *cptr;
+int main(void) {            // Este programa muestra la direccion de memoria de cada byte de una variable int
+    int data=9;             // y su valor en hexadecimal
+    int *iptr;              // Se declara un apuntador a un entero y un apuntador a un caracter
+    char *cptr;             // Se declara un apuntador a un caracter
 
-    iptr = &data;
-    cptr = (char *) &data;
+    iptr = &data;           // Se asigna la direccion de memoria de la variable data al apuntador iptr
+    cptr = (char *) &data;  // Se asigna la direccion de memoria de la variable data al apuntador cptr
 
     printf("==================================================================\n");
     printf("| Value of data = %d hex value = %x\n", data, data);
@@ -16,7 +16,7 @@ int main(void) {
     printf("| Character pointer pointing at %p\n", cptr);
     printf("| Printing address of all the four bytes of variable int data\n");
 
-    for(int i=0; i<4; i++) {
+    for(int i=0; i<4; i++) {  // Se imprime la direccion de memoria de cada byte de la variable data
         printf("| Address: %p, have %x\n", cptr, *cptr);
         cptr++;
     }
