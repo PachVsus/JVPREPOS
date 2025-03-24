d2d54fcd57239799df56b19701a693c812aab171
JesusVargasPacheco <jesus.vargas@iteso.mx>
2025-03-24 10:11:23 -0600

    Mejorar la legibilidad del código en uso_de_variables_condicionales.c mediante la adición de comentarios explicativos y la reestructuración de la lógica de manejo de hilos.

--- Porgramas de clase/C/segundo_parcial/uso_de_variables_condicionales.c
+++ Porgramas de clase/C/segundo_parcial/uso_de_variables_condicionales.c
@@ -1,26 +1,26 @@
-#include <pthread.h>
-#include <time.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <pthread.h>    // For pthread_create, pthread_cond_timedwait, pthread_mutex_lock, pthread_mutex_unlock, pthread_cond_signal, pthread_t, pthread_mutex_t, pthread_cond_t
+#include <time.h>   // For time
+#include <errno.h>  // For ETIMEDOUT
+#include <stdio.h>  // For printf
+#include <stdlib.h> // For atoi
+#include <unistd.h> // For sleep
 
-struct my_struct_tag {
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-    int value;
+struct my_struct_tag {      // Structure to store the mutex, condition variable, and value
+    pthread_mutex_t mutex;  // Mutex to protect the shared data
+    pthread_cond_t cond;    // Condition variable to signal the main thread
+    int value;              // Value to store the predicate
 };
 
-struct my_struct_tag data = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, 0};
+struct my_struct_tag data = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, 0};   // Initialize the structure with default values
 
 int hibernation = 2; /* Default to 1 second */
 
 /* Thread start routine. It will set the main thread’s predicate
    and signal the condition variable.
 */
-void *wait_thread(void *arg) {
-    srand(time(NULL));
-    sleep(hibernation);
+void *wait_thread(void *arg) {  // Function to be executed by the wait_thread
+    srand(time(NULL));          // Seed the random number generator
+    sleep(hibernation);         // Sleep for the specified number of seconds
 
     /* This function is running in a separate thread. After some
        simulated work, this thread also attempts to lock the same
@@ -28,34 +28,34 @@ void *wait_thread(void *arg) {
        main thread, this function cannot immediately acquire the lock.
        It will block (wait) until the main thread unlocks the mutex.
     */
-    pthread_mutex_lock(&data.mutex);
-    printf("\tLock mutex inside wait_thread.\n");
-    data.value = rand()%5+1; /* Set predicate */
+    pthread_mutex_lock(&data.mutex);    // Lock the mutex
+    printf("\tLock mutex inside wait_thread.\n");   // Print a message to indicate that the mutex has been locked
+    data.value = rand()%5+1; // Set predicate to a random number between 1 and 5
 
-    pthread_cond_signal(&data.cond);
-    printf("\tSignaling condition.\n");
+    pthread_cond_signal(&data.cond);    // Signal the condition variable
+    printf("\tSignaling condition.\n"); // Print a message to indicate that the condition has been signaled
 
-    pthread_mutex_unlock(&data.mutex);
-    printf("\tUnlock mutex inside wait_thread.\n");
+    pthread_mutex_unlock(&data.mutex); // Unlock the mutex
+    printf("\tUnlock mutex inside wait_thread.\n"); // Print a message to indicate that the mutex has been unlocked
 
-    return NULL;
+    return NULL;    // Return NULL to indicate that the thread has finished
  }
 
-int main (int argc, char *argv[]) {
-    int status;
-    pthread_t wait_thread_id;
-    struct timespec timeout;
+int main (int argc, char *argv[]) { // Main function with command line arguments
+    int status;                     // Variable to store the status of the condition wait
+    pthread_t wait_thread_id;       // Variable to store the ID of the wait_thread
+    struct timespec timeout;        // Variable to store the timeout value
 
     /* If an argument is specified, interpret it as the number
        of seconds for wait_thread to sleep before signaling the
        condition variable. You can play with this to see the
        condition wait below time out or wake normally.
     */
-    if (argc > 1)
-        hibernation = atoi(argv[1]);
+    if (argc > 1)   // Check if an argument was provided
+        hibernation = atoi(argv[1]);    // Convert the argument to an integer and store it in hibernation
 
     /* Create wait_thread. */
-    pthread_create(&wait_thread_id, NULL, wait_thread, NULL);
+    pthread_create(&wait_thread_id, NULL, wait_thread, NULL);   // Create a new thread and store its ID in wait_thread_id
     printf("Create wait thread.\n");
 
     /* Wait on the condition variable for 2 seconds, or until
@@ -63,16 +63,16 @@ int main (int argc, char *argv[]) {
        should signal. If you raise "hibernation" above 2
        seconds, it will time out.
     */
-    timeout.tv_sec = time(NULL) + 3;
-    timeout.tv_nsec = 0;
+    timeout.tv_sec = time(NULL) + 3;    // Set the timeout to 3 seconds
+    timeout.tv_nsec = 0;                // Set the nanoseconds to 0
 
     /*  mutex locked before calling pthread_cond_timewait, this ensures that no other
         can modify the shared data.value variable while the main thread checks its
         condition and waits on the condition variable. The mutex remains locked while
         the main thread waits for the condition to be signaled.
     */
-    pthread_mutex_lock(&data.mutex);
-    printf("Lock mutex.\n");
+    pthread_mutex_lock(&data.mutex);    // Lock the mutex
+    printf("Lock mutex.\n");        // Print a message to indicate that the mutex has been locked
 
     while(data.value == 0) {
         /* Once the main thread enters the pthread_cond_timedwait(), it performs two
@@ -84,11 +84,14 @@ int main (int argc, char *argv[]) {
            can the modify data.value and signal the condition.
         */
         status = pthread_cond_timedwait(&data.cond, &data.mutex, &timeout);
-        if (status == ETIMEDOUT) {
-            printf("Condition wait timed out.\n");
+        if (status == ETIMEDOUT) {                  // Check if the condition wait timed out
+            printf("Condition wait timed out.\n");  // Print a message to indicate that the condition wait timed out
             break;
-        } else if (status != 0)
-            printf("Wait on condition: %d\n");
+        } else if (status != 0)                     // Check if the condition wait failed
+            printf("Wait on condition: %d\n");     // Print a message to indicate that the condition wait failed
+            perror("pthread_cond_timedwait");   // Print an error message if the condition wait fails
+            exit(1);    // Exit the program with an error code
+
     }
 
     /* After the wait_thread() signals the condition and unlocks the mutex, the main thread
@@ -97,22 +100,22 @@ int main (int argc, char *argv[]) {
        re-locks the mutex and proceeds to check the value of data.value to determine which
        condition was signaled.
     */
-    if(data.value == 1)
-        printf ("Condition 1 was signaled.\n");
-    else if(data.value == 2)
-        printf ("Condition 2 was signaled.\n");
-    else if(data.value == 3)
-        printf ("Condition 3 was signaled.\n");
-    else if(data.value == 4)
-        printf ("Condition 4 was signaled.\n");
-    else if(data.value == 5)
-        printf ("Condition 5 was signaled.\n");
-    else
-        printf ("Unrecognized condition.\n");
 
-    pthread_mutex_unlock(&data.mutex);
-    printf("Unlock mutex.\n");
+    if(data.value == 1) // Check if the value of data is 1
+        printf ("Condition 1 was signaled.\n"); // Print a message to indicate that condition 1 was signaled
+    else if(data.value == 2)    // Check if the value of data is 2
+        printf ("Condition 2 was signaled.\n"); // Print a message to indicate that condition 2 was signaled
+    else if(data.value == 3)    // Check if the value of data is 3
+        printf ("Condition 3 was signaled.\n"); // Print a message to indicate that condition 3 was signaled
+    else if(data.value == 4)    // Check if the value of data is 4
+        printf ("Condition 4 was signaled.\n"); // Print a message to indicate that condition 4 was signaled
+    else if(data.value == 5)    // Check if the value of data is 5
+        printf ("Condition 5 was signaled.\n"); // Print a message to indicate that condition 5 was signaled
+    else                  // If the value of data is not recognized
+        printf ("Unrecognized condition.\n");   // Print a message to indicate that the condition is not recognized
+
+    pthread_mutex_unlock(&data.mutex);  // Unlock the mutex
+    printf("Unlock mutex.\n");    // Print a message to indicate that the mutex has been unlocked
 
     return 0;
 }
-
