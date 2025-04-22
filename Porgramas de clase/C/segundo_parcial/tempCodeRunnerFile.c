4bb88fae565fb4dc3135779bd20ae5fa971c6963
JesusVargasPacheco <jesus.vargas@iteso.mx>
2025-04-22 12:03:53 -0600

    feat: implementar productor-consumidor utilizando hilos en C

--- Porgramas de clase/C/segundo_parcial/tempCodeRunnerFile.c
+++ Porgramas de clase/C/segundo_parcial/tempCodeRunnerFile.c
@@ -1,107 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
+#include <pthread.h>    // Para usar hilos
+#include <time.h>   // Para la función rand()
+#include <unistd.h>  // Para la función sleep()
 
-/*Crea un programa en C que toma una matriz cuadrada M de dimensión 'n' ('n' renglones x 'n' columnas) y la eleve a la potencia 'k'.
+#define BUFFER_SIZE 5   // Tamaño del buffer circular
+#define TOTAL_ITEMS 15  // Total de elementos a producir y consumir
 
-El programa debe hacer uso de hilos para elevar la matriz 'M' a la potencia 'k'*/
+// Estructura de datos compartida
+struct {
+    int buffer[BUFFER_SIZE];  // Buffer circular de 5 elementos
+    int count;                // Número de elementos en el buffer
+    int in;                   // Índice para producir siguiente elemento
+    int out;                  // Índice para consumir siguiente elemento
+    pthread_mutex_t mutex;      // Mutex para proteger el acceso al buffer
+    pthread_cond_t cond_producer;   // Condición para el productor
+    pthread_cond_t cond_consumer;   // Condición para el consumidor
+} production_line = { {0}, 0, 0, 0, PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER };    // Inicialización de la estructura
 
-// Se crea la estructura de la matriz
-typedef struct {
-    int **matriz; // Matriz de enteros, ** significa que es un apuntador a un apuntador
-    int n; // Dimension de la matriz
-    int k; // Potencia a la que se elevara la matriz
-} matriz_t; // Se crea un alias para la estructura. termina en _t porque es un tipo de dato definido por el usuario
+// Función del productor
+void* producer(void* arg) {                 // Inicializar el productor
+    for (int i = 0; i < TOTAL_ITEMS; i++) {             // Producir TOTAL_ITEMS elementos
+        pthread_mutex_lock(&production_line.mutex);     // Bloquear el mutex para acceder al buffer
 
-// Funcion que eleva una matriz a una potencia
-void* elevar_matriz(void* arg) {
-    matriz_t* matriz = (matriz_t*)arg; // El argumento se convierte a un apuntador a matriz_t
-    int** temp = (int**)malloc(matriz->n * sizeof(int*)); // Matriz temporal
-    for (int i = 0; i < matriz->n; i++) {
-        temp[i] = (int*)malloc(matriz->n * sizeof(int));
+
+ while (production_line.count == BUFFER_SIZE) {       // Comprobar si el buffer está lleno
+        // Esperar si el buffer está lleno
+        pthread_cond_wait(&production_line.cond_producer, &production_line.mutex);
     }
 
-    for (int i = 0; i < matriz->n; i++) {
-        for (int j = 0; j < matriz->n; j++) {
-            temp[i][j] = 0;
-            for (int l = 0; l < matriz->n; l++) {
-                temp[i][j] += matriz->matriz[i][l] * matriz->matriz[l][j];
-            }
-        }
-    }
+    // Producir un elemento y añadirlo al buffer
+    production_line.buffer[production_line.in] = i + 1; // Producir un elemento (i+1)
+    printf("Producer: produced %d\n", production_line.buffer[production_line.in]);  // Mostrar el elemento producido
+    production_line.in = (production_line.in + 1) % BUFFER_SIZE;    // Actualizar el índice de producción
+    production_line.count++;    // Incrementar el contador de elementos en el buffer
+    printf("\tThere are %d elements in the buffer.\n", production_line.count);  // Mostrar el número de elementos en el buffer
 
-    for (int i = 0; i < matriz->n; i++) {
-        for (int j = 0; j < matriz->n; j++) {
-            matriz->matriz[i][j] = temp[i][j];
-        }
-    }
+    // Señalar al consumidor que hay nuevos datos disponibles
+    pthread_cond_signal(&production_line.cond_consumer);    // Señalar al consumidor que hay nuevos datos disponibles
+    printf("\tSignaling that there are products ready to be consumed.\n");  // Señalar al consumidor que hay nuevos datos disponibles
 
-    for (int i = 0; i < matriz->n; i++) {
-        free(temp[i]);
-    }
-    free(temp);
+    pthread_mutex_unlock(&production_line.mutex);   // Desbloquear el mutex
+    sleep(rand()%3);  // Simular tiempo de producción   
+}
+return NULL;
 
-    pthread_exit(NULL);
+
 }
 
-// Funcion para imprimir una matriz
-void imprimir_matriz(int** matriz, int n) {
-    for (int i = 0; i < n; i++) {       // Se recorre la matriz por renglones
-        for (int j = 0; j < n; j++) {       // Se recorre la matriz por columnas
-            printf("%d ", matriz[i][j]); // Se imprime la matriz
-        }
-        printf("\n");
+// Función del consumidor
+void* consumer(void* arg) {
+    for (int i = 0; i < TOTAL_ITEMS; i++) {            // Consumir TOTAL_ITEMS elementos
+        pthread_mutex_lock(&production_line.mutex);    // Bloquear el mutex para acceder al buffer
+
+
+ while (production_line.count == 0) {     // Comprobar si el buffer está vacío
+        // Esperar hasta que el buffer tenga datos
+        pthread_cond_wait(&production_line.cond_consumer, &production_line.mutex);  // Esperar hasta que el buffer tenga datos
     }
+
+    // Consumir el elemento del buffer
+    int item = production_line.buffer[production_line.out]; // Consumir el elemento del buffer
+    printf("Consumer: consumed %d\n", item);    // Mostrar el elemento consumido
+    production_line.out = (production_line.out + 1) % BUFFER_SIZE;  // Actualizar el índice de consumo
+    production_line.count--;    // Decrementar el contador de elementos en el buffer
+    printf("\tThere are %d elements in the buffer.\n", production_line.count);  // Mostrar el número de elementos en el buffer
+
+    // Señalar al productor que hay espacio disponible
+    pthread_cond_signal(&production_line.cond_producer);    // Señalar al productor que hay espacio disponible
+    printf("\tSignaling that there are slots ready to be filled.\n");   // Señalar al productor que hay espacio disponible
+
+    pthread_mutex_unlock(&production_line.mutex);   // Desbloquear el mutex
+    sleep(rand()%3);    // Simular tiempo de consumo
+}
+return NULL;    // Función del consumidor
+
+
 }
 
-// Funcion main
+// Función principal
 int main() {
-    int n;  // Dimension de la matriz
-    int k;  // Potencia a la que se elevara la matriz
-    pthread_t* hilos = NULL;   // Hilos
-    matriz_t matriz;    // Matriz
+    srand(time(NULL));  // Inicializar la semilla para la función rand()
+    // Inicializar el mutex y las variables de condición
+    pthread_t prod_thread, cons_thread;
 
-    printf("Elevacion de matriz con hilos\n");  // Nombre del programa
-    printf("Ingrese la dimension de la matriz (n): ");
-    scanf("%d", &n);
-    printf("Ingrese la potencia a la que se elevara la matriz (k): ");
-    scanf("%d", &k);
 
-    matriz.n = n;   // Se asigna la dimension de la matriz
-    matriz.k = k;   // Se asigna la potencia a la que se elevara la matriz
+ // Crear hilos productor y consumidor
+    pthread_create(&prod_thread, NULL, producer, NULL); // Crear hilo productor
+    pthread_create(&cons_thread, NULL, consumer, NULL); // Crear hilo consumidor
 
-    matriz.matriz = (int**)malloc(n * sizeof(int*)); // Reserva memoria para la matriz
-    for (int i = 0; i < n; i++) {
-    hilos = (pthread_t*)malloc((k - 1) * sizeof(pthread_t)); // Reserva memoria para los hilos
-    for (int p = 1; p < k; p++) {   // Se recorre la potencia a la que se elevara la matriz
-    }
-    for (int p = 1; p < k; p++) {   // Se recorre la potencia a la que se elevara la matriz
-        pthread_create(&hilos[p - 1], NULL, elevar_matriz, (void*)&matriz); // Se crean los hilos
-        pthread_join(hilos[p - 1], NULL);   // Se espera a que los hilos terminen
+    // Esperar a que ambos hilos terminen
+    pthread_join(prod_thread, NULL);    // Esperar a que el hilo productor termine
+    pthread_join(cons_thread, NULL);    // Esperar a que el hilo consumidor termine
 
-        // Se imprime la matriz después de cada multiplicación
-        printf("Matriz M elevada a la potencia %d:\n", p + 1);
-        imprimir_matriz(matriz.matriz, n);
-    }
-    printf("Matriz M elevada a la potencia k = %d:\n", k);
-    for (int i = 0; i < n; i++) {       // Se recorre la matriz por renglones
-        for (int j = 0; j < n; j++) {       // Se recorre la matriz por columnas
-            printf("%d ", matriz.matriz[i][j]); // Se imprime la matriz
-        }
-        printf("\n");
-    }
+    // Limpieza de recursos
+    pthread_mutex_destroy(&production_line.mutex);  // Destruir el mutex
+    pthread_cond_destroy(&production_line.cond_producer);   // Destruir la variable de condición del productor
+    pthread_cond_destroy(&production_line.cond_consumer);   // Destruir la variable de condición del consumidor
+    printf("All items produced and consumed.\n");  // Mostrar mensaje de finalización
 
-    // Se libera la memoria de la matriz
-    for (int i = 0; i < n; i++) {
-        free(matriz.matriz[i]); // Se libera la memoria de las columnas
-    }
-    free(matriz.matriz); // Se libera la memoria de los renglones
+    return 0;   // Fin de la función principal
 
-    // Se libera la memoria de los hilos
-    free(hilos); // Se libera la memoria de los hilos
-    }
-
-    return 0;
 
 }
